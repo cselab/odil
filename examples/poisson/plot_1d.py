100fc430795fb3d5d3c8eb9d2309c7c881aac431
Sergey Litvinov <slitvinov@gmail.com>
2023-11-08 18:48:55 -0500

    final 1d

--- examples/poisson/plot_1d.py
+++ examples/poisson/plot_1d.py
@@ -1,69 +1,60 @@
 #!/usr/bin/env python3
 """
-1D poisson
+1D Poisson
 ==========
 """
 
-import argparse
-import numpy as np
 import odil
 import matplotlib.pyplot as plt
 
-def apply_bc_u_mod(st, iw, nw, mod):
+def apply_bc(domain, st):
+    ix, = domain.indices()
+    nx, = domain.size()
+    mod = domain.mod
     extrap = odil.core.extrap_quadh
-    st[1] = mod.where(iw[0] == 0, extrap(st[2], st[0], 0), st[1])
-    st[2] = mod.where(iw[0] == nw[0] - 1, extrap(st[1], st[0], 0), st[2])
+    st[1] = mod.where(ix == 0, extrap(st[2], st[0], 0), st[1])
+    st[2] = mod.where(ix == nx - 1, extrap(st[1], st[0], 0), st[2])
+
 
 def operator(ctx):
-    extra = ctx.extra
-    mod = ctx.domain.mod
-    dw = ctx.step()
-    iw = ctx.indices()
-    nw = ctx.size()
-    u_st = [ctx.field("u"), ctx.field("u", -1), ctx.field("u", 1)]
-    apply_bc_u_mod(u_st, iw, nw, mod=mod)
-    u, uwm, uwp = u_st
-    u_ww = [(uwp - 2 * u + uwm) / dw[0]**2]
-    return [sum(u_ww) - extra.rhs]
+    dx, = ctx.step()
+    u_st = [ctx.field('u'), ctx.field('u', -1), ctx.field('u', 1)]
+    apply_bc(ctx.domain, u_st)
+    u_xx = (u_st[2] - 2 * u_st[0] + u_st[1]) / dx**2
+    return [u_xx - ctx.extra['rhs']]
+
+def get_ref_u(x):
+    p = 5
+    v = 5 * (1 - x) * x
+    return (v**p / (1 + v**p))**(1 / p)
 
 N = 32
-p = 5
-parser = argparse.ArgumentParser()
-odil.util.add_arguments(parser)
-odil.linsolver.add_arguments(parser)
-parser.set_defaults(multigrid=1)
-parser.set_defaults(optimizer='lbfgsb')
-args = parser.parse_args()
-odil.setup_outdir(args)
-domain = odil.Domain(cshape=[N],
-                     multigrid=args.multigrid,
-                     dtype=np.float64)
+domain = odil.Domain(cshape=[N], multigrid=True)
 mod = domain.mod
-x, = domain.points(loc='c')
-u0 = 5 * (1 - x) * x
-ref_u = (u0**p / (1 + u0**p))**(1 / p)
-dw = domain.step()
-iw = domain.indices()
-nw = domain.size()
+x, = domain.points()
+dx, = domain.step()
+ref_u = get_ref_u(x)
 u_st = [ref_u, mod.roll(ref_u, 1, 0), mod.roll(ref_u, -1, 0)]
-apply_bc_u_mod(u_st, iw, nw, mod)
-u_ww = [(u_st[2] - 2 * u_st[0] + u_st[1]) / dw[0]**2]
-rhs = sum(u_ww)
-state = odil.State()
-state.fields['u'] = None
+apply_bc(domain, u_st)
+rhs = (u_st[2] - 2 * u_st[0] + u_st[1]) / dx**2
+state = odil.State(fields={'u': None})
 state = domain.init_state(state)
-extra = argparse.Namespace()
-extra.ref_u = ref_u
-extra.rhs = rhs
-extra.args = args
-problem = odil.Problem(operator, domain, extra)
-callback = odil.make_callback(problem, args)
-odil.util.optimize(args, args.optimizer, problem, state, callback)
+problem = odil.Problem(operator, domain, extra={'rhs': rhs})
+opt = odil.optimizer.make_optimizer('lbfgsb', dtype=domain.dtype, mod=mod)
+
+
+def loss_grad(arrays):
+    domain.arrays_to_state(arrays, state)
+    loss, grads, terms, names, norms = problem.eval_loss_grad(state)
+    pinfo = {'terms': terms, 'names': names, 'norms': norms, 'loss': loss}
+    return loss, grads, pinfo
+
+
+opt.run(domain.arrays_from_state(state), loss_grad=loss_grad, epochs=33)
 fig, ax = plt.subplots()
-x = domain.points(0)
-u = domain.field(state, "u")
+u = domain.field(state, 'u')
 ax.plot(x, u, 'o', label='ODIL')
-ax.plot(x, extra.ref_u, label='reference')
+ax.plot(x, ref_u, label='reference')
 ax.set_xlabel('x')
 ax.set_ylabel('u')
 plt.show()
