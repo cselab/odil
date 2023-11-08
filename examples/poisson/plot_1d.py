437b1689c2e2afabd55160e21893ad3c4a7471ce
Sergey Litvinov <slitvinov@gmail.com>
2023-11-08 18:02:41 -0500

    plot_1d: fix

--- examples/poisson/plot_1d.py
+++ examples/poisson/plot_1d.py
@@ -24,34 +24,19 @@ def apply_bc_u_mod(st, iw, nw, mod):
 
 
 def operator(ctx):
-    domain = ctx.domain
     extra = ctx.extra
-    args = extra.args
-    mod = domain.mod
-    ndim = domain.ndim
-    dirs = range(ndim)
+    mod = ctx.domain.mod
     dw = ctx.step()
     iw = ctx.indices()
     nw = ctx.size()
-
-    def stencil_var(key):
-        st = [ctx.field(key)]
-        for i in dirs:
-            w = [-1 if j == i else 0 for j in dirs]
-            st.append(ctx.field(key, *w))
-            w = [1 if j == i else 0 for j in dirs]
-            st.append(ctx.field(key, *w))
-        return st
-
-    u_st = stencil_var('u')
+    u_st = [ctx.field("u"), ctx.field("u", -1), ctx.field("u", 1)]
     apply_bc_u_mod(u_st, iw, nw, mod=mod)
     u, uwm, uwp = split_wm_wp(u_st)
-    u_ww = [(uwp[i] - 2 * u + uwm[i]) / dw[i]**2 for i in dirs]
-    fu = sum(u_ww) - extra.rhs
-    res = [fu]
-    return res
+    u_ww = [(uwp[0] - 2 * u + uwm[0]) / dw[0]**2]
+    return [sum(u_ww) - extra.rhs]
 
 N = 32
+p = 5
 parser = argparse.ArgumentParser()
 odil.util.add_arguments(parser)
 odil.linsolver.add_arguments(parser)
@@ -65,16 +50,14 @@ domain = odil.Domain(cshape=[N],
                      dtype=np.float64)
 mod = domain.mod
 x, = domain.points(loc='c')
-p = 5
-u = (1 - x) * x * 5
-ref_u = (u**p / (1 + u**p))**(1 / p)
+u0 = 5 * (1 - x) * x
+ref_u = (u0**p / (1 + u0**p))**(1 / p)
 dw = domain.step()
 iw = domain.indices()
 nw = domain.size()
-u_st = [u, mod.roll(u, 1, 0), mod.roll(u, -1, 0)]
+u_st = [ref_u, mod.roll(ref_u, 1, 0), mod.roll(ref_u, -1, 0)]
 apply_bc_u_mod(u_st, iw, nw, mod)
-u, uwm, uwp = split_wm_wp(u_st)
-u_ww = [(uwp[0] - 2 * u + uwm[0]) / dw[0]**2]
+u_ww = [(u_st[2] - 2 * u_st[0] + u_st[1]) / dw[0]**2]
 rhs = sum(u_ww)
 state = odil.State()
 state.fields['u'] = None
@@ -89,7 +72,7 @@ odil.util.optimize(args, args.optimizer, problem, state, callback)
 fig, ax = plt.subplots()
 x = domain.points(0)
 u = domain.field(state, "u")
-ax.plot(x, u, label='ODIL')
+ax.plot(x, u, 'o', label='ODIL')
 ax.plot(x, extra.ref_u, label='reference')
 ax.set_xlabel('x')
 ax.set_ylabel('u')
