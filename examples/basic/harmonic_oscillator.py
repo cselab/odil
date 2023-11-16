85e7889c28fa5998af9e204185c906ed1650d9b0
Sergey Litvinov <slitvinov@gmail.com>
2023-11-15 23:12:31 -0500

    better NN and data

--- examples/basic/harmonic_oscillator.py
+++ examples/basic/harmonic_oscillator.py
@@ -4,34 +4,33 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import math
 
+
 def oscillator(d, w0, x):
     w = math.sqrt(w0**2 - d**2)
     phi = math.atan2(-d, w)
     y = torch.exp(-d * x) * torch.cos(phi + w * x) / math.cos(phi)
     return y
 
-torch.set_default_tensor_type(torch.FloatTensor)
+
+torch.manual_seed(123)
+torch.set_default_dtype(torch.float32)
 d, w0 = 2.0, 20.0
 x = torch.linspace(0, 1, 500)[:, None]
 y = oscillator(d, w0, x)
-x_data = x[0:200:20]
-y_data = y[0:200:20]
-x_physics = torch.linspace(0, 1, 30)[:, None].requires_grad_(True)
-torch.manual_seed(123)
-model = nn.Sequential(nn.Linear(1, 32), nn.Tanh(),  #
-                      nn.Linear(32, 16), nn.Tanh(), #
-                      nn.Linear(16, 1),)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+x_data = x[0:20:10]
+y_data = y[0:20:10]
+x_physics = torch.linspace(0, 1, 25)[:, None].requires_grad_(True)
+model = nn.Sequential(nn.Linear(1, 16), nn.Tanh(), nn.Linear(16, 16), nn.Tanh(), nn.Linear(16, 1))
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
 ones = torch.ones_like(x_physics)
-for i in range(20000):
+for i in range(5000):
     optimizer.zero_grad()
-    loss1 = torch.mean((model(x_data) - y_data)**2)
     yhp = model(x_physics)
     dx, = torch.autograd.grad(yhp, x_physics, ones, create_graph=True)
     dx2, = torch.autograd.grad(dx, x_physics, ones, create_graph=True)
     physics = dx2 + 2 * d * dx + w0 * w0 * yhp
-    loss2 = 1e-4 * torch.mean(physics**2)
-    loss = loss1 + loss2
+    loss = torch.mean(
+        (model(x_data) - y_data)**2) + 1e-4 * torch.mean(physics**2)
     loss.backward()
     optimizer.step()
 plt.plot(x, y)
